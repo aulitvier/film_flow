68e02e3022000d9379d5a8da69fee2574340f707
Olivier EVRARD <olivier.evrard29@gmail.com>
2023-10-31 11:21:04 +0100

    initiation du projet

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+import { HomePage } from './components/homePage';
+
+export const App = () => {
+
+  return(
+    <HomePage/>
+  );
+}
---
+++ src/components/homePage.jsx
@@ -0,0 +1,12 @@
+
+
+
+export const HomePage = () => {
+
+
+
+
+    return(
+        <p>ma Homepage</p>
+    )
+}
